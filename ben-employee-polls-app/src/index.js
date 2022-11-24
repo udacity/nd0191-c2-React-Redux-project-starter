1cd48aaafd635cb65a806ed2564fb141fabab4ed
“Ben <benbright065@gmail.com>
2022-11-24 03:44:00 +0100

    Added new imports

--- ben-employee-polls-app/src/index.js
+++ ben-employee-polls-app/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { legacy_createStore as createStore } from "redux";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/index";
+import middleware from "./middleware";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const store = createStore(rootReducer, middleware);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
